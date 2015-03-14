31bd51a3fc97796d6415f564f2308ab265bcc654
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2015-03-14 18:46:26 +0100

    libvirt/inject_key and libvirt/inject_partition already set in nova::compute::libvirt (values match with defaults)

--- manifests/compute/hypervisor.pp
+++ manifests/compute/hypervisor.pp
@@ -391,8 +391,9 @@ Host *
 
   # Extra config for nova-compute
   nova_config {
-    'libvirt/inject_key':            value => false;
-    'libvirt/inject_partition':      value => '-2';
+    # FIXME: already declared in nova::compute::libvirt
+#    'libvirt/inject_key':            value => false;
+#    'libvirt/inject_partition':      value => '-2';
     'libvirt/live_migration_flag':   value => 'VIR_MIGRATE_UNDEFINE_SOURCE,VIR_MIGRATE_PEER2PEER,VIR_MIGRATE_LIVE,VIR_MIGRATE_PERSIST_DEST';
     'libvirt/block_migration_flag':  value => 'VIR_MIGRATE_UNDEFINE_SOURCE,VIR_MIGRATE_PEER2PEER,VIR_MIGRATE_LIVE,VIR_MIGRATE_NON_SHARED_INC';
   }
