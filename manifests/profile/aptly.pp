d68d2a4ae64ffbec13380255737d1e4d816371bc
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2015-03-22 12:38:12 +0100

    Add aptly profile

---
+++ manifests/profile/aptly.pp
@@ -0,0 +1,15 @@
+class cloud::profile::aptly {
+
+  # ----- Setup apache for serving apt repos managed by aptly
+
+  class { 'apache':
+    default_mods    => false,
+    default_vhost   => false,
+    purge_configs   => true,
+  }
+
+  apache::vhost { 'puppetmaster.cloud.gwg.de':
+    port          => '80',
+    docroot       => '/var/lib/aptly/public',
+  }
+}
