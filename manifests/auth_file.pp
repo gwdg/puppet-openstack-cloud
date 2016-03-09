08d6e43ef2ad4a763cb5a7791a67cf4f687e103f
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2016-03-09 21:55:55 +0100

    Simplify / improve the openrc file: * Use Keystone v3 * Use admin token xor not * Use correct params for openstack client * Do not setup unnecessary params

--- manifests/auth_file.pp
+++ manifests/auth_file.pp
@@ -1,4 +1,4 @@
-# == Class: openstack::resources::auth_file
+# == Class: cloud::auth_file
 #
 # Creates an auth file that can be used to export
 # environment variables that can be used to authenticate
@@ -6,48 +6,29 @@
 #
 # === Parameters
 #
-# [*admin_password*]
-#   (required) Admin password.
-# [*controller_node*]
+# [*password*]
+#   (required) Password.
+# [*keystone_host*]
 #   (optional) Keystone address. Defaults to '127.0.0.1'.
-# [*keystone_admin_token*]
+# [*admin_token*]
 #   (optional) Admin token.
 #   NOTE: This setting will trigger a warning from keystone.
 #   Authentication credentials will be ignored by keystone client
 #   in favor of token authentication. Defaults to undef.
-# [*admin_user*]
+# [*User*]
 #   (optional) Defaults to 'admin'.
-# [*admin_tenant*]
-#   (optional) Defaults to 'openstack'.
-# [*region_name*]
-#   (optional) Defaults to 'RegionOne'.
-# [*use_no_cache*]
-#   (optional) Do not use the auth token cache. Defaults to true.
-# [*cinder_endpoint_type*]
-#   (optional) Defaults to 'publicURL'.
-# [*glance_endpoint_type*]
-#   (optional) Defaults to 'publicURL'.
-# [*keystone_endpoint_type*]
-#   (optional) Defaults to 'publicURL'.
-# [*nova_endpoint_type*]
-#   (optional) Defaults to 'publicURL'.
-# [*neutron_endpoint_type*]
-#   (optional) Defaults to 'publicURL'.
-#
+# [*Project*]
+#   (optional) Defaults to 'admin'.
+# [*interface_type*]
+#   (optional) Defaults to 'internal'.
 
 class cloud::auth_file(
-  $admin_password,
-  $controller_node          = '127.0.0.1',
-  $keystone_admin_token     = undef,
-  $admin_user               = 'admin',
-  $admin_tenant             = 'openstack',
-  $region_name              = 'RegionOne',
-  $use_no_cache             = true,
-  $cinder_endpoint_type     = 'publicURL',
-  $glance_endpoint_type     = 'publicURL',
-  $keystone_endpoint_type   = 'publicURL',
-  $nova_endpoint_type       = 'publicURL',
-  $neutron_endpoint_type    = 'publicURL',
+  $keystone_host    = '127.0.0.1',
+  $admin_token      = undef,
+  $user             = 'admin',
+  $password         = undef,
+  $project          = 'admin',
+  $interface_type   = 'internal'
 ) {
 
   include ::openstacklib::openstackclient
