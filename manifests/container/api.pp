8eabd9fc18ca6b4bbd187ab31ca9bf11666ad3c0
Maik Srba <maik.srba@gwdg.de>
2016-04-07 12:09:16 +0200

    Clean magnum api

--- manifests/container/api.pp
+++ manifests/container/api.pp
@@ -1,5 +1,4 @@
 #
-#
 class cloud::container::api(
   $ks_magnum_internal_port    = 9511,
   $ks_keystone_internal_host  = '127.0.0.1',
@@ -10,17 +9,17 @@ class cloud::container::api(
   $ks_magnum_password         = 'magnumpassword',
 ){
 
-	include 'cloud::container'
+  include 'cloud::container'
 
-	class { 'magnum::api':
-		admin_password => $ks_magnum_password,
-		host           => $api_eth,
-    	auth_uri       => "${ks_keystone_internal_proto}://${ks_keystone_internal_host}:${ks_keystone_internal_port}/v2.0",
-  		identity_uri   => "${ks_keystone_internal_proto}://${ks_keystone_internal_host}:${ks_keystone_admin_port}",
-    	port           => $ks_magnum_internal_port,
-  	}
+  class { 'magnum::api':
+    admin_password => $ks_magnum_password,
+    host           => $api_eth,
+    auth_uri       => "${ks_keystone_internal_proto}://${ks_keystone_internal_host}:${ks_keystone_internal_port}/v2.0",
+    identity_uri   => "${ks_keystone_internal_proto}://${ks_keystone_internal_host}:${ks_keystone_admin_port}",
+    port           => $ks_magnum_internal_port,
+  }
 
-	@@haproxy::balancermember{"${::fqdn}-magnum_api":
+  @@haproxy::balancermember{"${::fqdn}-magnum_api":
     listening_service => 'magnum_api',
     server_names      => $::hostname,
     ipaddresses       => $api_eth,
