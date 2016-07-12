18f92a892d90d4bad1a4c972300fdfca905a72c0
Maik Srba <msrba@gwdg.de>
2016-07-12 10:54:39 +0200

    change order for aodh api

--- manifests/role/spof.pp
+++ manifests/role/spof.pp
@@ -9,10 +9,10 @@ class cloud::role::spof inherits ::cloud::role::base {
     class { '::cloud::telemetry::centralagent': }        ->
 
     class { '::cloud::telemetry::alarming::engine': }    ->
+    class { '::cloud::telemetry::alarming::api': }       ->
     class { '::cloud::telemetry::alarming::evaluator': } ->
     class { '::cloud::telemetry::alarming::notifier': }  ->
     class { '::cloud::telemetry::alarming::listener': }  ->
-    class { '::cloud::telemetry::alarming::api': }       ->
     class { '::cloud::telemetry::alarming::client': }    ->
     
     class { '::cloud::telemetry::collector': }           ->
