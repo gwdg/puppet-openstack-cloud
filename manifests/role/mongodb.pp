89571be473548bf978fa5089c3cfcee8465c05ff
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2016-08-13 16:09:32 +0200

    Fix missing systemd service for mongodb v3.2.8

--- manifests/role/mongodb.pp
+++ manifests/role/mongodb.pp
@@ -1,11 +1,14 @@
 class cloud::role::mongodb inherits ::cloud::role::base {
 
-    class { '::mongodb::globals':
-#        manage_package_repo => true,
-    }                                                       ->
-    class { '::mongodb::server': }                          ->
-    class { '::mongodb::client': }                          ->
-    class { '::mongodb::replset': }
+  class { '::mongodb::globals': }               ->
+  class { '::mongodb::server': }                ->
+  class { '::mongodb::client': }                ->
+  class { '::mongodb::replset': }
 
-#    class { '::cloud::database::nosql::mongodb::mongod': }
+  # Fix mongodb v3.2.8 
+  file { '/lib/systemd/system/mongod.service':
+    content => file('cloud/patches/mongodb_3.2.8_fix_no_systemd_service_file.txt'),
+    before  => Class['mongodb::server::service'],
+    require => Class['mongodb::server::install']
+  }
 }
