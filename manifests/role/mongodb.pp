d3b7fc476a2ff1908551d4b8e93107529384902b
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2016-08-14 10:50:06 +0200

    Move systemd fix for mongodb v3.2.8 to mongodb module

--- manifests/role/mongodb.pp
+++ manifests/role/mongodb.pp
@@ -1,3 +1,4 @@
+#
 class cloud::role::mongodb inherits ::cloud::role::base {
 
   class { '::mongodb::globals': }               ->
@@ -5,10 +6,4 @@ class cloud::role::mongodb inherits ::cloud::role::base {
   class { '::mongodb::client': }                ->
   class { '::mongodb::replset': }
 
-  # Fix mongodb v3.2.8 
-  file { '/lib/systemd/system/mongod.service':
-    content => file('cloud/patches/mongodb_3.2.8_fix_no_systemd_service_file.txt'),
-    before  => Class['mongodb::server::service'],
-    require => Class['mongodb::server::install']
-  }
 }
