244f764b67d8a00398f52bcf1eac52da0e602c8d
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2016-08-15 18:17:00 +0200

    Allow to use Keystone v3 api for neutron_admin_auth_url

--- manifests/compute.pp
+++ manifests/compute.pp
@@ -129,7 +129,7 @@ class cloud::compute(
 
   class { '::nova::network::neutron':
     neutron_admin_password => $neutron_password,
-    neutron_admin_auth_url => "${neutron_protocol}://${neutron_endpoint}:35357/v2.0",
+    neutron_admin_auth_url => "${neutron_protocol}://${neutron_endpoint}:35357",
     neutron_auth_plugin    => 'password',
     neutron_url            => "${neutron_protocol}://${neutron_endpoint}:9696",
     neutron_region_name    => $neutron_region_name
