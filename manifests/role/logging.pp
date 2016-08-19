1000786183064681eb9684c9c0644ff8c29aa6da
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2016-08-19 16:15:04 +0200

    Role adaptations for influxdb

--- manifests/role/logging.pp
+++ manifests/role/logging.pp
@@ -2,6 +2,7 @@
 class cloud::role::logging inherits ::cloud::role::base {
 
   	class { '::cloud::profile::logstash': }       ->
-  	class { '::cloud::profile::kibana': }
+  	class { '::cloud::profile::kibana': }         ->
+  	class { '::cloud::profile::influxdb': }
 
 }
