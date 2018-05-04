bd33cc59fccbd17c985ca8d9275898e8f56faaa9
Maik Srba <msrba@gwdg.de>
2018-05-04 13:27:02 +0200

    change from deploy memcache in role identity-api to hiera parameter keystone::resource::authtoken::manage_memcache_package

--- manifests/role/identity_api.pp
+++ manifests/role/identity_api.pp
@@ -2,8 +2,6 @@
 class cloud::role::identity_api inherits ::cloud::role::base {
 
     class { '::cloud::auth_file': }                     ->
-
-    class { '::cloud::profile::memcache': }             ->
     
     class { '::cloud::identity': }
 }
