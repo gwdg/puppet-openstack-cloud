07eed864810f50f3c5ef0854217aa88bb9116bfd
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2018-05-18 16:19:26 +0200

    remove memcache profile from network_api role - use manage_memcache_package now

--- manifests/role/network_api.pp
+++ manifests/role/network_api.pp
@@ -3,8 +3,6 @@ class cloud::role::network_api inherits ::cloud::role::base {
 
     class { '::cloud::auth_file': }                     ->
 
-    class { '::cloud::profile::memcache': }             ->
-    
     class { '::cloud::network::controller': }           ->
     
     class { '::cloud::network::qos': }
