70a05faff9a53b2c741a1e84149b166a21de948d
Maik Srba <msrba@gwdg.de>
2018-06-19 11:18:56 +0200

    remove cloud::profile::memcache from volume_api role

--- manifests/role/volume_api.pp
+++ manifests/role/volume_api.pp
@@ -2,8 +2,6 @@
 class cloud::role::volume_api inherits ::cloud::role::base {
 
     class { '::cloud::auth_file': }                     ->
-
-    class { '::cloud::profile::memcache': }             ->
     
     class { '::cloud::volume::scheduler': }             ->
     class { '::cloud::volume::api': }
