647b50d336d6658787b6ef0a65da7870d11a6d53
Maik Srba <msrba@gwdg.de>
2018-06-19 11:19:29 +0200

    move cinder::keystone::authtoken parameters to hiera data

--- manifests/volume.pp
+++ manifests/volume.pp
@@ -34,17 +34,6 @@
 #
 class cloud::volume(
 
-  $ks_cinder_internal_port    = 8776,
-
-  $ks_cinder_user             = 'cinder',
-  $ks_cinder_password         = 'cinderpassword',
-  $ks_admin_tenant            = 'services',
-
-  $ks_keystone_internal_host  = '127.0.0.1',
-  $ks_keystone_internal_proto = 'http',
-  $ks_keystone_internal_port  = 5000,
-  $ks_keystone_admin_port     = 35357,
-
   $rabbit_hosts               = ['127.0.0.1:5672'],
   $rabbit_password            = 'rabbitpassword',
 
@@ -63,18 +52,6 @@ class cloud::volume(
     rabbit_password           => $rabbit_password,
     rabbit_virtual_host       => '/',
     storage_availability_zone => $storage_availability_zone
-  }
-
-  class { '::cinder::keystone::authtoken':
-
-    username                       => $ks_cinder_user,
-    password                       => $ks_cinder_password,
-    project_name                   => $ks_admin_tenant,
-
-    auth_url                       => "${ks_keystone_internal_proto}://${ks_keystone_internal_host}:${ks_keystone_admin_port}",
-    auth_uri                       => "${ks_keystone_internal_proto}://${ks_keystone_internal_host}:${ks_keystone_internal_port}",
-
-    memcached_servers              => $memcache_servers,
   }
 
   class { '::cinder::ceilometer': }
