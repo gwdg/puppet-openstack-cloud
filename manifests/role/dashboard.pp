057231a981908cc49b143ade8a6625fceb90ef5c
Maik Srba <msrba@gwdg.de>
2018-06-20 15:50:32 +0200

    python-memcache is now installed by horizon module

--- manifests/role/dashboard.pp
+++ manifests/role/dashboard.pp
@@ -2,8 +2,6 @@
 class cloud::role::dashboard inherits ::cloud::role::base {
 
     class { '::cloud::auth_file': }                     ->
-
-    class { '::cloud::profile::memcache': }             ->
     
     class { '::cloud::dashboard': }
 }
