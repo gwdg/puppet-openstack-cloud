a031757da3e7b4ea73c826b6d340d4add4c87bdd
Maik Srba <msrba@gwdg.de>
2018-07-09 11:17:18 +0200

    implement openidc for fedcloud

--- manifests/role/identity_api.pp
+++ manifests/role/identity_api.pp
@@ -3,5 +3,7 @@ class cloud::role::identity_api inherits ::cloud::role::base {
 
     class { '::cloud::auth_file': }                     ->
     
-    class { '::cloud::identity': }
+    class { '::cloud::identity': }                      ->
+
+    class { '::cloud::identity::openidc': }
 }
