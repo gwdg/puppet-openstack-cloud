44b700c124914639fdfe934b10a80ce59aa96f33
Maik Srba <msrba@gwdg.de>
2019-06-05 12:53:51 +0200

    move to new heat::keystone::authtoken

--- manifests/orchestration.pp
+++ manifests/orchestration.pp
@@ -45,14 +45,9 @@
 #
 class cloud::orchestration(
 
-  $auth_uri                   = 'http://127.0.0.1:5000/',
-  $identity_uri               = 'http://127.0.0.1:35357/',
-
   $ks_heat_public_host        = '127.0.0.1',
   $ks_heat_public_proto       = 'http',
   $ks_heat_password           = 'heatpassword',
-
-  $memcache_servers           = [],
 
   $rabbit_hosts               = ['127.0.0.1:5672'],
   $rabbit_password            = 'rabbitpassword',
@@ -64,14 +59,6 @@ class cloud::orchestration(
   include ::heat::db
 
   class { '::heat':
-
-    auth_plugin           => 'password',
-    identity_uri          => $identity_uri,
-    auth_uri              => $auth_uri,
-    keystone_user         => 'heat',
-    keystone_password     => $ks_heat_password,
-    memcached_servers     => $memcache_servers,
-
     rabbit_hosts          => $rabbit_hosts,
     rabbit_password       => $rabbit_password,
     rabbit_userid         => 'heat',
@@ -82,6 +69,8 @@ class cloud::orchestration(
     manage_user   => false,
     manage_role   => false,
   }
+
+  class { '::heat::client': }
 
   heat_config {
     'clients/endpoint_type': value => $os_endpoint_type;
