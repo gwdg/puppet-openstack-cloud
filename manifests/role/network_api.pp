53c1b0f416a854d5c1cc1ac0c45acedf79333e45
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2019-07-03 17:02:03 +0200

    fix syntax error

--- manifests/role/network_api.pp
+++ manifests/role/network_api.pp
@@ -3,5 +3,5 @@ class cloud::role::network_api inherits ::cloud::role::base {
 
     class { '::cloud::auth_file': }                     ->
 
-    class { '::cloud::network::controller': }           ->
+    class { '::cloud::network::controller': }
 }
