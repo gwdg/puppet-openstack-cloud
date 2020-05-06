f687d6a958abf1a0c1ef337991c0cd026a2ef08d
Maik Srba <msrba@gwdg.de>
2020-05-06 16:05:21 +0200

    remove profile memcached from orchestration_api role because it will be installed by keystone

--- manifests/role/orchestration_api.pp
+++ manifests/role/orchestration_api.pp
@@ -2,8 +2,6 @@
 class cloud::role::orchestration_api inherits ::cloud::role::base {
 
     class { '::cloud::auth_file': }                     ->
-
-    class { '::cloud::profile::memcache': }             ->
     
     class { '::cloud::orchestration::engine': }         ->
     class { '::cloud::orchestration::api': }
