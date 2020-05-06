afdf7f53c9c393b00dc7c008032705c6202cc984
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2020-05-06 16:02:53 +0200

    remove memached role from orchestartion_api role because it will be installed over keystone

--- manifests/role/orchestration_api.pp
+++ manifests/role/orchestration_api.pp
@@ -3,8 +3,6 @@ class cloud::role::orchestration_api inherits ::cloud::role::base {
 
     class { '::cloud::auth_file': }                     ->
 
-    class { '::cloud::profile::memcache': }             ->
-    
     class { '::cloud::orchestration::engine': }         ->
     class { '::cloud::orchestration::api': }
 }
