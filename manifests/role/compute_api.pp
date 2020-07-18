f8dab9f58f0ee054779ab6ae28acbfb8ff7d12b4
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2020-07-18 14:00:55 +0200

    Remove cloud::compute::cert (not used any more / deprecated)

--- manifests/role/compute_api.pp
+++ manifests/role/compute_api.pp
@@ -4,10 +4,9 @@ class cloud::role::compute_api inherits ::cloud::role::base {
     class { '::cloud::auth_file': }                     ->
     
     class { '::cloud::compute::conductor': }            ->
-    class { '::cloud::compute::cert': }                 ->
     class { '::cloud::compute::consoleauth': }          ->
     class { '::cloud::compute::consoleproxy': }         ->
     class { '::cloud::compute::api': }                  ->
-    class { '::cloud::compute::placement': }                  ->
+    class { '::cloud::compute::placement': }            ->
     class { '::cloud::compute::scheduler': }
 }
